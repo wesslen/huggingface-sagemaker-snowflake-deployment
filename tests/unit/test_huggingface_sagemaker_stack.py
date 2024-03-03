4c54eb801fd8034d624bdf500637c1fb9c9c4235
Ryan Wesslen <ryan@explosion.ai>
2024-03-03 14:31:22 -0500

    Update test_huggingface_sagemaker_stack.py

--- tests/unit/test_huggingface_sagemaker_stack.py
+++ tests/unit/test_huggingface_sagemaker_stack.py
@@ -1,3 +1,4 @@
+from aws_cdk import Environment
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
@@ -7,6 +8,8 @@ from huggingface_sagemaker.huggingface_sagemaker_stack import HuggingfaceSagemak
 # resource in huggingface_sagemaker_snowflake_deployment/huggingface_sagemaker_snowflake_deployment_stack.py
 def test_sqs_queue_created():
     app = core.App()
+    # env = Environment(account='123456789012', region='us-west-2')
+    # stack = HuggingfaceSagemakerStack(app, "huggingface-sagemaker", env=env)
     stack = HuggingfaceSagemakerStack(app, "huggingface-sagemaker")
     template = assertions.Template.from_stack(stack)
 
